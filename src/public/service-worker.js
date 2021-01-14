4b1aca764d2be20eb41691fc1ef784fa0aa38dd8
Chris Leveille <cdleveille@gmail.com>
2021-01-14 02:57:37 -0500

    reversed caching to first try fetching and then fall back on cache if needed

--- src/public/service-worker.js
+++ src/public/service-worker.js
@@ -77,21 +77,22 @@ self.addEventListener('install', function (event) {
 });
 
 self.addEventListener('fetch', function (event) {
-	event.respondWith(caches.match(event.request).then(function (response) {
-		// caches.match() always resolves
-		// but in case of success response will have value
+	event.respondWith(fetch(event.request).then(function (response) {
+
 		if (response !== undefined) {
+			// response may be used only once
+			// we need to save clone to put one copy in cache
+			// and serve second one
+			let responseClone = response.clone();
+
+			caches.open('v1').then(function (cache) {
+				cache.put(event.request, responseClone);
+			});
 			return response;
 		} else {
-			return fetch(event.request).then(function (response) {
-				// response may be used only once
-				// we need to save clone to put one copy in cache
-				// and serve second one
-				let responseClone = response.clone();
-
-				caches.open('v1').then(function (cache) {
-					cache.put(event.request, responseClone);
-				});
+			return caches.match(event.request).then(function (response) {
+				// caches.match() always resolves
+				// but in case of success response will have value
 				return response;
 			}).catch(function () {
 				return caches.match('/sw-test/gallery/myLittleVader.jpg');
