10e0ac0e533180d8d9e56abbb79ae936a521de30
JudeBoy <judegiordano@gmail.com>
2021-01-14 17:43:54 -0500

    Use EJS as View Template

--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -1,11 +1,12 @@
 import * as dotenv from 'dotenv';
-import { Environment as Env } from '../types/Constants';
+import { Environment as Env, ShouldCache } from '../types/Constants';
 
 dotenv.config();
 
 const config = {
 	PORT: <number>parseInt(process.env.PORT) || 3000,
-	ENV: <Env>process.env.NODE_ENV || Env.dev
+	ENV: <Env>process.env.NODE_ENV || Env.dev,
+	SHOULD_CACHE: <ShouldCache>process.env.SHOULD_CACHE || ShouldCache.false
 };
 
 export default config;
