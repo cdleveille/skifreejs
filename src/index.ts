b0a9adc70fb72343d1c888024da4fb44c6f0ff7f
JudeBoy <judegiordano@gmail.com>
2021-01-15 12:47:44 -0500

    Basic CRUD and ErrorHandling

--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,21 @@ import express, { Request, Response } from 'express';
 import app from './controllers/index';
 import { cwd } from 'process';
 import log from './services/logger';
+import errorHandler from './middleware/errorHandler';
 
 app.use(express.static(path.join(cwd(), './public/')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(cwd(), './public/'));
 
-app.get('/', async (req: Request, res: Response): Promise<void> => {
+// serve static
+app.get('/', (req: Request, res: Response): void => {
 	return res.status(200).render('ski.ejs', {
 		shouldCache: config.SHOULD_CACHE
 	});
 });
+
+// this must be referenced last
+app.use(errorHandler);
 
 const start = async (): Promise<void> => {
 	app.listen(config.PORT, () => {
