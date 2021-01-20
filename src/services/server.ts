6dd458145082fe2dcae0725b6996f1cb4ca6acb2
JudeBoy <judegiordano@gmail.com>
2021-01-20 00:01:59 -0500

    Refactor Helmet for 'Unsafe Inline' / Add Cors

--- src/services/server.ts
+++ src/services/server.ts
@@ -5,6 +5,7 @@ import responseTime from 'response-time';
 import helmet from 'helmet';
 import compression from 'compression';
 import morgan from 'morgan';
+import cors from 'cors';
 import { Environment as Env } from '../types/Constants';
 
 const app = express();
@@ -14,13 +15,14 @@ app.set('json spaces', 2);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(compression());
+app.use(cors());
 /* eslint-disable quotes */
 app.use(
 	helmet.contentSecurityPolicy({
 		directives: {
 			"default-src": ["'self'"],
 			"object-src": ["'none'"],
-			"script-src": ["'self'", "code.jquery.com", "cdnjs.cloudflare.com"],
+			"script-src": ["'self'", "'unsafe-inline'", "code.jquery.com", "cdnjs.cloudflare.com"],
 			"style-src": ["'self'", "'unsafe-inline'", "cdnjs.cloudflare.com", "fonts.googleapis.com"],
 			"font-src": ["'self'", "'unsafe-inline'", "fonts.googleapis.com", "fonts.gstatic.com"]
 		},
