681563d1c1c5c2598f4614bf465e5b0a0083d92f
Chris Leveille <cdleveille@gmail.com>
2021-01-20 21:52:48 -0500

    first attempt at mobile controls

--- public/js/input.js
+++ public/js/input.js
@@ -1,86 +1,124 @@
 /* eslint-disable no-undef */
 export default class InputHandler {
 	constructor(canvas, game) {
-		canvas.addEventListener('mousemove', (event) => {
-			game.mousePos.x = event.clientX - ((window.innerWidth - canvas.width) / 2);
-			game.mousePos.y = event.clientY - ((window.innerHeight - canvas.height) / 2);
-		});
+		if (!game.util.isOnMobile())
+		{
+			canvas.addEventListener('mousemove', (event) => {
+				game.mousePos.x = event.clientX - ((window.innerWidth - canvas.width) / 2);
+				game.mousePos.y = event.clientY - ((window.innerHeight - canvas.height) / 2);
+			});
 
-		canvas.addEventListener('mousedown', (event) => {
-			if (!game.isPaused) {
-				if (event.button == 0) {
-					if (!game.skier.isCrashed) {
-						if (game.skier.isJumping) {
-							game.skier.rotateJumpStage();
+			canvas.addEventListener('mousedown', (event) => {
+				if (!game.isPaused) {
+					if (event.button == 0) {
+						if (!game.skier.isCrashed) {
+							if (game.skier.isJumping) {
+								game.skier.rotateJumpStage();
+							}
 						}
-					}
-				} else if (event.button == 2 && !game.skier.trick1Disabled) {
-					if (game.skier.isJumping && !game.skier.isCrashed) {
-						game.skier.isDoingTrick1 = true;
-						game.skier.trick1Disabled = true;
-						game.skier.trick1StartTime = game.util.timestamp();
+					} else if (event.button == 2 && !game.skier.trick1Disabled) {
+						if (game.skier.isJumping && !game.skier.isCrashed) {
+							game.skier.isDoingTrick1 = true;
+							game.skier.trick1Disabled = true;
+							game.skier.trick1StartTime = game.util.timestamp();
+						}
 					}
 				}
-			}
-		});
+			});
 
-		canvas.addEventListener('mouseup', (event) => {
-			if (!game.isPaused) {
-				if (event.button == 0) {
-					if (!game.skier.isCrashed) {
-						if (!game.skier.isJumping) {
-							game.skier.isJumping = true;
-							game.skier.jumpV = game.skier.jumpVInit;
+			canvas.addEventListener('mouseup', (event) => {
+				if (!game.isPaused) {
+					if (event.button == 0) {
+						if (!game.skier.isCrashed) {
+							if (!game.skier.isJumping) {
+								game.skier.isJumping = true;
+								game.skier.jumpV = game.skier.jumpVInit;
+							}
+						} else if (game.skier.isStopped) {
+							game.skier.isCrashed = false;
 						}
-					} else if (game.skier.isStopped) {
+					} else if (event.button == 2) {
+						game.skier.isDoingTrick1 = false;
+						game.skier.trick1EndTime = game.util.timestamp();
+					}
+				}
+			});
+
+			let left = 65, right = 68, p = 80, r = 82;
+
+			document.addEventListener('keydown', (event) => {
+				switch (event.keyCode) {
+				case left:
+					if (game.skier.isStopped) {
+						game.skier.isSkatingLeft = true;
+					}
+					break;
+				case right:
+					if (game.skier.isStopped) {
+						game.skier.isSkatingRight = true;
+					}
+					break;
+				}
+			});
+
+			document.addEventListener('keyup', (event) => {
+				switch (event.keyCode) {
+				case left:
+					game.skier.isSkatingLeft = false;
+					break;
+				case right:
+					game.skier.isSkatingRight = false;
+					break;
+				case p:
+					if (game.isPaused) {
+						game.startTime += (game.util.timestamp() - game.timePausedAt);
+						game.isPaused = false;
+					} else {
+						game.drawIsPaused = false; // will be flipped to true immediately in game.draw()
+						game.timePausedAt = game.util.timestamp();
+						game.isPaused = true;
+					}
+					break;
+				case r:
+					game.init();
+					break;
+				}
+			});
+		} else {
+			canvas.addEventListener('touchstart', (event) => {
+				if (!game.isPaused) {
+					game.touchStartTime = game.util.timestamp();
+					let touch = event.touches[0];
+					game.mousePos.x = touch.clientX - ((window.innerWidth - canvas.width) / 2);
+					game.mousePos.y = touch.clientY - ((window.innerHeight - canvas.height) / 2);
+					if (game.skier.isCrashed) {
 						game.skier.isCrashed = false;
 					}
-				} else if (event.button == 2) {
-					game.skier.isDoingTrick1 = false;
-					game.skier.trick1EndTime = game.util.timestamp();
 				}
-			}
-		});
+			});
 
-		let left = 65, right = 68, p = 80, r = 82;
+			canvas.addEventListener('touchmove', (event) => {
+				if (!game.isPaused) {
+					let touch = event.touches[0];
+					game.mousePos.x = touch.clientX - ((window.innerWidth - canvas.width) / 2);
+					game.mousePos.y = touch.clientY - ((window.innerHeight - canvas.height) / 2);
+				}
+			});
 
-		document.addEventListener('keydown', (event) => {
-			switch (event.keyCode) {
-			case left:
-				if (game.skier.isStopped) {
-					game.skier.isSkatingLeft = true;
+			canvas.addEventListener('touchend', () => {
+				if (!game.isPaused) {
+					if (game.util.timestamp() - game.touchStartTime < 200) {
+						if (!game.skier.isCrashed) {
+							if (!game.skier.isJumping) {
+								game.skier.isJumping = true;
+								game.skier.jumpV = game.skier.jumpVInit;
+							} else {
+								game.skier.rotateJumpStage();
+							}
+						}
+					}
 				}
-				break;
-			case right:
-				if (game.skier.isStopped) {
-					game.skier.isSkatingRight = true;
-				}
-				break;
-			}
-		});
-
-		document.addEventListener('keyup', (event) => {
-			switch (event.keyCode) {
-			case left:
-				game.skier.isSkatingLeft = false;
-				break;
-			case right:
-				game.skier.isSkatingRight = false;
-				break;
-			case p:
-				if (game.isPaused) {
-					game.startTime += (game.util.timestamp() - game.timePausedAt);
-					game.isPaused = false;
-				} else {
-					game.drawIsPaused = false; // will be flipped to true immediately in game.draw()
-					game.timePausedAt = game.util.timestamp();
-					game.isPaused = true;
-				}
-				break;
-			case r:
-				game.init();
-				break;
-			}
-		});
+			});
+		}
 	}
 }
--- public/js/util.js
+++ public/js/util.js
@@ -92,4 +92,26 @@ export default class Util {
 			});
 		});
 	}
+
+	// determine whether the user is playing on a desktop or mobile device
+	isOnMobile() {
+		let isMobile = {
+			Android: function() {
+				return navigator.userAgent.match(/Android/i);
+			},
+			BlackBerry: function() {
+				return navigator.userAgent.match(/BlackBerry/i);
+			},
+			iOS: function() {
+				return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+			},
+			Opera: function() {
+				return navigator.userAgent.match(/Opera Mini/i);
+			},
+			Windows: function() {
+				return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
+			}
+		};
+		return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+	}
 }
