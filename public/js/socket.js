d3981952cbea9c5265ac56dcc4f720a2a5a96a8f
JudeBoy <judegiordano@gmail.com>
2021-01-24 20:42:15 -0500

    Globalize Socket Var in Client

--- public/js/socket.js
+++ public/js/socket.js
@@ -1,1 +1,3 @@
-var socket = io.connect({ 'reconnectionDelay': 1000, 'reconnectionAttempts': 10 });
+const socket = io.connect({ 'reconnectionDelay': 1000, 'reconnectionAttempts': 10 });
+
+export default socket;
